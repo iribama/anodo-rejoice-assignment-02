a1fc05472c4a4d7dcae0a8212f0cb82287190c97
Rejoice Anodo <101342958+iribama@users.noreply.github.com>
2022-07-05 17:17:07 +0000

    Challenge 2 done.

--- src/assignment.js
+++ src/assignment.js
@@ -34,8 +34,11 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-  let count
-    return arrayOfNumbers.length;
+  let count= 0;
+  for(i = 0; i < arrayOfNumbers.length; i++) {
+    arrayOfNumbers[i] % 2 == 0 && count++;
+  }
+    return count;
 }
  assignment.countEvenNumbers = countEvenNumbers;
 
